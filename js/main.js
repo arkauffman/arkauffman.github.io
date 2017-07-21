f4be1bfe0f8572cbcae932898cf8b234f812b2d9
Alexandra Kauffman <arkauffma@gmail.com>
2017-07-20 22:35:11 -0700

    updated styling, images, and about me page

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// var length = $(".heart").length;
+// if(length <= 25){
+//     length++;
+//         // don't append
+// } else {
+//     $("#heart").append('<i id="heart"class="fa fa-heart-o" aria-hidden="true"></i>');
+// }
+
+
+// var heart = $('.heart')
+
+// for (var i = 0; i <= 25; i++) {
+//     $('#heart').append(heart);
+// }
