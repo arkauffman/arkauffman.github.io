dd135309d0a1806d518b849cd939c26e18e1ab33
Alexandra Kauffman <arkauffma@gmail.com>
2017-09-11 18:15:53 -0700

    added jquery scroll functionality to index

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
-// var length = $(".heart").length;
-// if(length <= 25){
-//     length++;
-//         // don't append
-// } else {
-//     $("#heart").append('<i id="heart"class="fa fa-heart-o" aria-hidden="true"></i>');
-// }
-
-
-// var heart = $('.heart')
-
-// for (var i = 0; i <= 25; i++) {
-//     $('#heart').append(heart);
-// }
+$(document).ready(function() {
+    
+    var aboveHeight = $('header').outerHeight();
+    //when scroll
+        $(window).scroll(function(){
+                if ($(window).scrollTop() > aboveHeight){
+                $('nav').addClass('fixed').css('top','0').next()
+                .css('padding-top','60px');
+                } else {
+                $('nav').removeClass('fixed').next()
+                .css('padding-top','0');
+                }
+            });
+        });
+    $('a[href*="#"]')
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+          &&
+          location.hostname == this.hostname
+        ) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              var $target = $(target);
+              $target.focus();
+            });
+          }
+        }
+    });
