2f747dabe252c84a9584c42b4a0041922d49efd1
Alexandra Kauffman <arkauffma@gmail.com>
2017-09-11 20:13:54 -0700

    added jquery single page nav bar

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
 $(document).ready(function() {
-    
     var aboveHeight = $('header').outerHeight();
-    //when scroll
-        $(window).scroll(function(){
-                if ($(window).scrollTop() > aboveHeight){
-                $('nav').addClass('fixed').css('top','0').next()
-                .css('padding-top','60px');
-                } else {
-                $('nav').removeClass('fixed').next()
-                .css('padding-top','0');
-                }
-            });
-        });
-    $('a[href*="#"]')
-      .not('[href="#"]')
-      .not('[href="#0"]')
-      .click(function(event) {
-        if (
-          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-          &&
-          location.hostname == this.hostname
-        ) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          if (target.length) {
-            event.preventDefault();
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 1000, function() {
-              var $target = $(target);
-              $target.focus();
-            });
-          }
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > aboveHeight){
+            $('nav').addClass('fixed').css('top','0').next()
+        } else {
+            $('nav').removeClass('fixed').next()
         }
     });
+});
+
+$('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+    ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            event.apreventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 2000, function() {
+                var $target = $(target);
+                $target.focus();
+            });
+        }
+    }
+});
